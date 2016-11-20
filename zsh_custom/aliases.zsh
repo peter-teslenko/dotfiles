78bedcd05dbd30871a882c6c96df6cba9e871110
Peter Teslenko <smartchecker@gmail.com>
2016-11-20 19:05:09 +0300

    add aliases.zsh

---
+++ zsh_custom/aliases.zsh
@@ -0,0 +1,25 @@
+# Shortcuts
+alias d="cd ~/Clouds/Dropbox"
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+# alias p="cd ~/projects"
+alias g="git"
+alias h="history"
+alias j="jobs"
+
+alias reload!='. ~/.zshrc'
+
+#alias c='pygmentize -O style=tomorrownighteighties -f console256 -g'
+
+# local webserver
+alias _w="open http://localhost:8000;python -m SimpleHTTPServer 8000"
+
+# tree aliases
+alias tree1="tree -L 1"
+alias tree1h="tree -L 1 -h"
+alias tree2="tree -L 2"
+alias tree2h="tree -L 2 -h"
+alias tree3="tree -L 3"
+alias tree3h="tree -L 3 -h"
+
+alias appleinfo="archey"
